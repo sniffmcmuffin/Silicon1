9f1e15f6c0ce27ef7dc28fc03e255105aa347a4b
sniffmcmuffin <sjostrom.jimmy@gmail.com>
2024-02-15 21:36:13 +0100

    Fxed some scss typos. Added header and footer. Added button model.

---
+++ Silicon1/Views/Components/ButtonViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Silicon1.Views.Components;
+
+public class ButtonViewModel
+{
+    public string? Text { get; set; }
+    public string? Icon { get; set; }
+    public string? ClassName { get; set; }
+    public string? Link { get; set; }
+}
