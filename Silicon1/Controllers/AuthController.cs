331290549882e5cb421dd5398dea8c66af7e651f
sniffmcmuffin <sjostrom.jimmy@gmail.com>
2024-02-21 21:32:27 +0100

    Signup init.

--- Silicon1/Controllers/AuthController.cs
+++ Silicon1/Controllers/AuthController.cs
@@ -4,4 +4,9 @@ namespace Silicon1.Controllers;
 
 public class AuthController : Controller
 {
+	[Route("/signup")] // Så kan gå direkt till signup i stället för /auth/signup/
+	public IActionResult SignUp()
+	{
+		return View();
+	}
 }
