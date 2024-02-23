305ddcbb7374c68b86ed9edbbec1675e81be948a
sniffmcmuffin <sjostrom.jimmy@gmail.com>
2024-02-23 19:08:05 +0100

    Signup complete.

---
+++ Silicon1/ViewModels/SignUpViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Silicon1.Models;
+
+namespace Silicon1.ViewModels;
+
+public class SignUpViewModel
+{
+	public string Title { get; set; } = "Sign up";
+	public SignUpModel Form { get; set; } = new SignUpModel();
+	//public bool TermsAndConditions { get; set; } = false;
+}
