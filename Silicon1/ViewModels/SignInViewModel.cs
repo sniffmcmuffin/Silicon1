f20ab165c29a120e0d06c2c9d8c9da77227bc64f
sniffmcmuffin <sjostrom.jimmy@gmail.com>
2024-02-24 18:40:43 +0100

    Sign in complete.

---
+++ Silicon1/ViewModels/SignInViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Silicon1.Models;
+
+namespace Silicon1.ViewModels;
+
+public class SignInViewModel
+{
+	public string Title { get; set; } = "Sign in";
+	public SignInModel Form { get; set; } = new SignInModel();
+	public string? ErrorMessage { get; set; }
+}
