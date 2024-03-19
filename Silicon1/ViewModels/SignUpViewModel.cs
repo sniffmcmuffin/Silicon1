502b14b318b79dcec3d59afeb99fe375e9c2d7dd
sniffmcmuffin <sjostrom.jimmy@gmail.com>
2024-03-19 21:57:53 +0100

    Started on a new project for Identity - Individual Accounts alternative.

--- Silicon1/ViewModels/SignUpViewModel.cs
+++ Silicon1/ViewModels/SignUpViewModel.cs
@@ -6,5 +6,5 @@ public class SignUpViewModel
 {
 	public string Title { get; set; } = "Sign up";
 	public SignUpModel Form { get; set; } = new SignUpModel();
-	//public bool TermsAndConditions { get; set; } = false;
+    public bool TermsAndConditions { get; set; } = false;
 }
---
+++ idInfrastructure/Entities/UserEntity.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace idInfrastructure.Entities;
+
+public class UserEntity : IdentityUser
+{
+    [ProtectedPersonalData]
+    public string FirstName { get; set; } = null!;
+
+    [ProtectedPersonalData]
+    public string LastName { get; set; } = null!;
+    public int? AddressId { get; set; }
+    public AddressEntity? Address { get; set; }
+}
