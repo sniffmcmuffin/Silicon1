e08059e71b50b68ae9de23109539e54333202c1e
sniffmcmuffin <sjostrom.jimmy@gmail.com>
2024-04-19 10:04:44 +0200

    Added Cookie Consent and Dark Mode. Dark mode is in need of further tweaking.

---
+++ Silicon1/Controllers/SettingsController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Silicon1.Controllers;
+
+public class SettingsController : Controller
+{
+	public IActionResult ChangeTheme(string theme)
+	{
+		var option = new CookieOptions
+		{
+			Expires = DateTime.Now.AddDays(60),
+		};
+
+		Response.Cookies.Append("ThemeMode", theme, option);
+		return Ok();
+	}
+
+	[HttpPost]
+	public IActionResult CookieConsent()
+	{
+		var option = new CookieOptions
+		{
+			Expires = DateTime.Now.AddYears(1),
+			HttpOnly = true,
+			Secure = true
+		};
+
+		Response.Cookies.Append("CookieConsent", "true", option);
+		return Ok();
+	}	
+}
